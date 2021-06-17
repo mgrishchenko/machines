52e2a64438d796e1da1aff785e20e64646e8a91c
Maria Grishchenko <grishchenko.m17@gmail.com>
2021-06-17 18:03:33 +0300

    ArrayList and Switch/case

--- src/com/solvd/body/Cabriolet.java
+++ src/com/solvd/body/Cabriolet.java
@@ -1,16 +1,37 @@
 package com.solvd.body;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cabriolet extends Carbody {
 
     private int speed;
     private String name;
     private String city;
 
+
+    public void showCabrioletNames () {
+        for (int i = 0; i < addCabrioletValues().size(); i++) {
+            System.out.println((i + 2) + addCabrioletValues().get(i));
+        }
+    }
+    public List <String> addCabrioletValues() {
+        List<String> SedanList = new ArrayList<String>();
+        SedanList.add("Lexus");
+        SedanList.add("Audi");
+        SedanList.add("Mini");
+        SedanList.add("Peugeot");
+        SedanList.add("Porshe");
+        return SedanList;
+    }
+
+
     public Cabriolet(int speed, String name, String city) {
         this.name = name;
         this.speed = speed;
         this.city = city;
     }
+
 
 
     public String getName() {
@@ -38,7 +59,7 @@ public class Cabriolet extends Carbody {
 
     }
 
-    public Cabriolet(String opel, String jack) {
+    public Cabriolet() {
 
     }
     public Cabriolet(String name, String driver, int year){
